11d830717ac21f20bdaff576f07a3e7a30fb1c25
NickyFieu <jantjefieu@gmail.com>
2020-10-28 12:37:44 +0100

    Cleaning up SkeletonDatausageSample

--- Code Samples/SocketSystem/SkeletalDataUsageSample.cpp
+++ Code Samples/SocketSystem/SkeletalDataUsageSample.cpp
@@ -11,47 +11,47 @@ void GoblinGCController::AddWeapon(GameScene& currentScene)
 
 	// check if the club material already exists if not create it
 	const UINT clubMaterialID = 39;
-	DaDfEmNmSp3D_ShadowMaterial* weaponMat = nullptr;
+	DaDfEmNmSp3D_ShadowMaterial* pWeaponMat = nullptr;
 	if (!gameContext.pMaterialManager->HasMaterial(clubMaterialID))
 	{
-		weaponMat = new DaDfEmNmSp3D_ShadowMaterial();
-		weaponMat->SetLightDirection(gameContext.pShadowMapper->GetLightDirection());
-		weaponMat->SetUseDiffuseMap(true);
-		weaponMat->SetDiffuseTexture(L"./Resources/Textures/GameTextures/GoblinWeapon_Diffuse.png");
-		weaponMat->SetUseNormalMap(true);
-		weaponMat->SetNormalTexture(L"./Resources/Textures/GameTextures/GoblinWeapon_Normal.png");
-		gameContext.pMaterialManager->AddMaterial(weaponMat, clubMaterialID);
+		pWeaponMat = new DaDfEmNmSp3D_ShadowMaterial();
+		pWeaponMat->SetLightDirection(gameContext.pShadowMapper->GetLightDirection());
+		pWeaponMat->SetUseDiffuseMap(true);
+		pWeaponMat->SetDiffuseTexture(L"./Resources/Textures/GameTextures/GoblinWeapon_Diffuse.png");
+		pWeaponMat->SetUseNormalMap(true);
+		pWeaponMat->SetNormalTexture(L"./Resources/Textures/GameTextures/GoblinWeapon_Normal.png");
+		gameContext.pMaterialManager->AddMaterial(pWeaponMat, clubMaterialID);
 	}
 
 	// create the model for the club
-	ModelComponent* model = new ModelComponent(L"./Resources/Meshes/Enemies/GoblinWeapon.ovm");
-	model->SetMaterial(clubMaterialID);
+	ModelComponent* pModel = new ModelComponent(L"./Resources/Meshes/Enemies/GoblinWeapon.ovm");
+	pModel->SetMaterial(clubMaterialID);
 
 	// get all the socket data needed to attach the club to a socket ( + extra transforms )
-	GameObject* socket = static_cast<BoneObject*>(m_pGameCharacterRef->GetWeaponSocket());
+	GameObject* pSocket = static_cast<BoneObject*>(m_pGameCharacterRef->GetWeaponSocket());
 	m_pWeaponSocketRef = new GameObject();
-	m_pWeaponSocketRef->AddComponent(model);
+	m_pWeaponSocketRef->AddComponent(pModel);
 	m_pWeaponSocketRef->GetTransform()->Scale(0.01f, 0.01f, 0.01f);
 	m_pWeaponSocketRef->GetTransform()->Rotate(25.f, 30.f, -15.f);
-	socket->AddChild(m_pWeaponSocketRef);
+	pSocket->AddChild(m_pWeaponSocketRef);
 
 	// create the collision for the club
-	physx::PxPhysics* physX = PhysxManager::GetInstance()->GetPhysics();
-	physx::PxMaterial* pMaterial = physX->createMaterial(0.f, 0.f, 0.f);
+	physx::PxPhysics* pPhysX = PhysxManager::GetInstance()->GetPhysics();
+	physx::PxMaterial* pMaterial = pPhysX->createMaterial(0.f, 0.f, 0.f);
 	std::shared_ptr<physx::PxGeometry> boxCollision{ new physx::PxBoxGeometry(0.8f, 1.5f, 0.8f) };
 
 	// add evrything we just created for the club to the club's gameobject / setting collision layers
 	m_pWeapon = new GameObject();
 
 	UINT32 collisionGroup = (UINT32)CollisionGroupFlag::Group1;
-	RigidBodyComponent* weaponRigidBody = new RigidBodyComponent(false, collisionGroup, ~collisionGroup);
-	weaponRigidBody->SetKinematic(true);
+	RigidBodyComponent* pWeaponRigidBody = new RigidBodyComponent(false, collisionGroup, ~collisionGroup);
+	pWeaponRigidBody->SetKinematic(true);
 
-	ColliderComponent* weaponCollisionBox = new ColliderComponent(boxCollision, *pMaterial, physx::PxTransform{ -0.50f,1.6f,0.f, physx::PxQuat::createIdentity() });
+	ColliderComponent* pWeaponCollisionBox = new ColliderComponent(boxCollision, *pMaterial, physx::PxTransform{ -0.50f,1.6f,0.f, physx::PxQuat::createIdentity() });
 	weaponCollisionBox->EnableTrigger(true);
 
-	m_pWeapon->AddComponent(weaponRigidBody);
-	m_pWeapon->AddComponent(weaponCollisionBox);
+	m_pWeapon->AddComponent(pWeaponRigidBody);
+	m_pWeapon->AddComponent(pWeaponCollisionBox);
 
 	// add the weapon to the scene 
 	currentScene.AddChild(m_pWeapon);
