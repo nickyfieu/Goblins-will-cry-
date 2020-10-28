e951865b02cd2272e88b4cfb0317cb6fc8da3de8
NickyFieu <jantjefieu@gmail.com>
2020-10-28 12:31:14 +0100

    Cleaning up meshfilter.cpp

--- Code Samples/SocketSystem/MeshFilterSample.cpp
+++ Code Samples/SocketSystem/MeshFilterSample.cpp
@@ -20,26 +20,38 @@ short MeshFilter::GetBoneIndexByName(const std::wstring& boneName) const
 {
 	// hashes the given bone name and tries to find if a valid skeleton data exists with that name and returns the index of that skeleton data
 	size_t boneNameHash = std::hash<std::wstring>{}(boneName);
-	auto it = std::find_if(m_SkeletonData.cbegin(), m_SkeletonData.cend(), [&boneNameHash](const SkeletonData* cmp) {return cmp->nameHash == boneNameHash; });
+	std::vector<SkeletonData*>::const_iterator it = std::find_if(m_SkeletonData.cbegin(), m_SkeletonData.cend(), [&boneNameHash](const SkeletonData* cmp) {return cmp->nameHash == boneNameHash; });
+	
 	if (it != m_SkeletonData.cend())
+	{
 		return (*it)->index;
+	}
 
-	Logger::LogWarning(L"MeshFilter::GetBoneIndexByName returning 0 as index due to invalid bonename:" + boneName);
+	Logger::LogWarning(L"MeshFilter::GetBoneIndexByName returning 0 as index due to invalid bonename:" +  boneName);
 	return 0;
 }
 
 SkeletonData* MeshFilter::GetSkeletonDataByIndex(short boneIndex) const
 {
 	// tries to find the skeleton data that has the given bone index and returns it
-	auto it = std::find_if(m_SkeletonData.cbegin(), m_SkeletonData.cend(), [&boneIndex](const SkeletonData* cmp) {return cmp->index == boneIndex; });
+	std::vector<SkeletonData*>::const_iterator it = std::find_if(m_SkeletonData.cbegin(), m_SkeletonData.cend(), [&boneIndex](const SkeletonData* cmp) {return cmp->index == boneIndex; });
+	
 	if (it != m_SkeletonData.cend())
+	{
 		return (*it);
+	}
 
-	Logger::LogWarning(L"MeshFilter::GetSkeletonDataByIndex nullpt due to unexistant bone index");
+	Logger::LogWarning(L"MeshFilter::GetSkeletonDataByIndex nullptr due to unexistant bone index");
 	return nullptr;
 }
 
 BoneObject* MeshFilter::GetBoneObjectByIndex(short boneIndex) const
 {
 	return m_BoneObjects[boneIndex];
+}
+
+SkeletonData::SkeletonData(const std::wstring& dataName)
+	: name{ dataName }
+{
+	nameHash = std::hash<std::wstring>{}(name);
 }
